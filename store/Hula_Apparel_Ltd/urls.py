da7e03d6102121111a5c5e9a8b31c388f53aeafa
achiengcindy <achiengcindy36@gmail.com>
2017-11-28 20:51:17 -0500

    setting enviroment

--- store/Hula_Apparel_Ltd/urls.py
+++ store/Hula_Apparel_Ltd/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^accounts/', include('allauth.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^profiles/', include('profiles.urls')),
+    #url(r'^contacts/$', views.contact, name='contacts'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
