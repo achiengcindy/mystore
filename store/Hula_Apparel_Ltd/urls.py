efc1b79ab4344b9af763c84cae8730757682d355
achiengcindy <achiengcindy36@gmail.com>
2017-11-29 13:50:58 -0500

    bootstrap looking Great

--- store/Hula_Apparel_Ltd/urls.py
+++ store/Hula_Apparel_Ltd/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     #url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^profiles/', include('profiles.urls')),
+    url(r'^profiles/', include('profiles.urls')),
     #url(r'^contacts/$', views.contact, name='contacts'),
 
 ]
--- store/profiles/models.py
+++ store/profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class profile(models.Model):
+	name = models.CharField(max_length=120)
+	description = models.TextField()
+	location = models.CharField(max_length=120,default='my location', blank=True, null=True)
+	job = models.CharField(max_length=120, null=True)
+	
+	def __str__(self):
+		return self.name
