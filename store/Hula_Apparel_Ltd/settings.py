a4aac8bc5e87aef92d9d069cfd6c36e2fdf5d415
achiengcindy <achiengcindy36@gmail.com>
2017-11-30 14:01:07 -0500

    contact form working well

--- store/Hula_Apparel_Ltd/settings.py
+++ store/Hula_Apparel_Ltd/settings.py
@@ -14,7 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_PORT='587'
+EMAIL_USE_TLS=True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -129,3 +133,4 @@ if DEBUG:
     STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static", "static"),
 )
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
