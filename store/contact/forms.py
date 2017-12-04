ae2362a6536a8491f5a56ec320a8090221111fb9
achiengcindy <achiengcindy36@gmail.com>
2017-12-04 16:58:04 -0500

    removed myshop and cntact

--- store/contact/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class contactForm(forms.Form):
-	name = forms.CharField(required=False, max_length=100)
-	email = forms.EmailField(required=True)
-	comment = forms.CharField(widget=forms.Textarea)
-	
